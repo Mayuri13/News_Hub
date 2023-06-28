da93343c51d0b270616730af8b013111d31a748c
Mayuri13 <mayuri.goswami2017@gmail.com>
2023-06-28 23:37:05 +0530

    Hide Api key

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,13 +2,14 @@ import React from 'react'
 import News from '../Components/News'
 
 export default function Home() {
+  const apiKey = process.env.REACT_APP_NEWS_API
   return (
     <>
       <div className="container-fluid bg-dark text-white d-flex justify-content-center align-items-center flex-column" style = {{height: "45vh"}}>
         <h2 style={{fontSize:"50px", color:"rgb(187 246 96)"}}>News Hub</h2>
         <h5 className='my-2'>Your trusted source for the latest headlines, delivered with unparalleled depth and speed.</h5>
       </div>
-      <News pageSize = {16}/>
+      <News apiKey={apiKey} pageSize = {16}/>
     </>
   )
 }
